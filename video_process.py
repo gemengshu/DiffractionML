5e92eb7223fc36875416ea4b9234472762849923
M. S. Ge <gemengshu@gmail.com>
2021-03-30 16:42:45 +0800

    Delete video_process.py

--- video_process.py
+++
@@ -1,7 +0,0 @@
-from utils import video_crop_to_images
-
-video_path = "D:/GMS/Documents/Dong/Co3O4/15_DP3.avi"
-rec = (30,4,512,512)
-save_path = "D:/GMS/Documents/Dong/data/15_DP3/"
-
-video_crop_to_images(video_path, rec, save_path)
